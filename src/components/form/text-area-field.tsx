a818f78448844acaf2f72590b37288492cb9086a
LeTanTru <letantru@gmail.com>
2025-11-15 17:48:47 +0700

    feat(video): create & update

--- src/components/form/text-area-field.tsx
+++ src/components/form/text-area-field.tsx
@@ -67,12 +67,12 @@ export default function TextAreaField<T extends FieldValues>({
             {label && (
               <FormLabel
                 htmlFor={id}
-                className={cn('mb-2 ml-0.5 gap-0', {
+                className={cn('mb-2 ml-2 gap-0', {
                   'origin-start text-muted-foreground/70 group-focus-within:text-foreground has-[+textarea:not(:placeholder-shown)]:text-foreground has-aria-invalid:border-destructive/60 has-aria-invalid:ring-destructive/20 dark:has-aria-invalid:ring-destructive/40 has-aria-invalid:border-destructive bg-background absolute top-0 block translate-y-2 cursor-text rounded px-1 text-sm transition-all group-focus-within:pointer-events-none group-focus-within:-translate-y-1/2 group-focus-within:cursor-default group-focus-within:text-xs group-focus-within:font-medium has-[+textarea:not(:placeholder-shown)]:pointer-events-none has-[+textarea:not(:placeholder-shown)]:-translate-y-1/2 has-[+textarea:not(:placeholder-shown)]:cursor-default has-[+textarea:not(:placeholder-shown)]:text-xs has-[+textarea:not(:placeholder-shown)]:font-medium':
                     floatLabel
                 })}
               >
-                <span className='px-1'>{label}</span>
+                {label}
                 {required && <span className='text-destructive ml-1'>*</span>}
               </FormLabel>
             )}
