338cefd20ff81de85dee6d8be3d1106e6debe67d
LeTanTru <letantru@gmail.com>
2026-04-13 20:20:48 +0700

    feat(config): add mqtt broker config support and adjust console removal to production

--- src/config.ts
+++ src/config.ts
@@ -14,7 +14,10 @@ const configSchema = z.object({
   NEXT_PUBLIC_APP_NAME: z.string(),
   NEXT_PUBLIC_GRANT_TYPE_REFRESH_TOKEN: z.string(),
   NEXT_PUBLIC_MEDIA_HOST: z.string(),
-  NEXT_PUBLIC_CLIENT_TYPE: z.string()
+  NEXT_PUBLIC_CLIENT_TYPE: z.string(),
+  NEXT_PUBLIC_MQTT_BROKER: z.url(),
+  NEXT_PUBLIC_MQTT_USERNAME: z.string(),
+  NEXT_PUBLIC_MQTT_PASSWORD: z.string()
 });
 
 const configProject = configSchema.safeParse({
@@ -31,7 +34,10 @@ const configProject = configSchema.safeParse({
   NEXT_PUBLIC_GRANT_TYPE_REFRESH_TOKEN:
     process.env.NEXT_PUBLIC_GRANT_TYPE_REFRESH_TOKEN,
   NEXT_PUBLIC_MEDIA_HOST: process.env.NEXT_PUBLIC_MEDIA_HOST,
-  NEXT_PUBLIC_CLIENT_TYPE: process.env.NEXT_PUBLIC_CLIENT_TYPE
+  NEXT_PUBLIC_CLIENT_TYPE: process.env.NEXT_PUBLIC_CLIENT_TYPE,
+  NEXT_PUBLIC_MQTT_BROKER: process.env.NEXT_PUBLIC_MQTT_BROKER,
+  NEXT_PUBLIC_MQTT_USERNAME: process.env.NEXT_PUBLIC_MQTT_USERNAME,
+  NEXT_PUBLIC_MQTT_PASSWORD: process.env.NEXT_PUBLIC_MQTT_PASSWORD
 });
 
 if (!configProject.success) {
