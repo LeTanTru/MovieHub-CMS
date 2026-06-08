012eb72900188ed767324304982e40253852dbad
LeTanTru <letantru@gmail.com>
2026-06-08 16:25:14 +0700

    refactor(subtitle): extract translation modal
    * move subtitle translation logic to a dedicated
      VideoLibrarySubtitleTranslateModal
    * simplify subtitle editing modal responsibilities
    * separate edit and translation modal state
      management
    * update component exports and imports

--- src/app/video-library/[id]/subtitle/_components/index.tsx
+++ src/app/video-library/[id]/subtitle/_components/index.tsx
@@ -1,2 +1,3 @@
 export { VideoLibrarySubtitleList } from './video-library-subtitle-list';
 export { VideoLibrarySubtitleModal } from './video-library-subtitle-modal';
+export { VideoLibrarySubtitleTranslateModal } from './video-library-subtitle-translate-modal';
